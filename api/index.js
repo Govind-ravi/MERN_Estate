db9c3fccd0f8a033d7e34aa0d29398e41db4e0af
Govind-ravi <govindnr20122001@gmail.com>
2024-07-04 02:51:25 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDb");
 }).catch((err)=>{
-    console.log(err);
+    console.log("DB connection error");
 })
 
 const app = express();
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("Server is running in 3000 port!");
 })
+
+app.use("/api/user", userRouter);
