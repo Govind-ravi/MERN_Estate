f7b5485a1ff482f562901a0320ca5e90d6a283aa
Govind-ravi <govindnr20122001@gmail.com>
2024-07-05 23:11:18 +0530

    Complete SignUp functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState();
+  const [loadind, setLoadind] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  }
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try {
+      
+      setLoadind(true);
+      const res = await fetch('api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if(data.success === false){
+        setError(data.message);
+        setLoadind(false);
+        return;
+      }
+      setLoadind(false);
+      setError(null);
+      navigate('/sign-in')
+    } catch (error) {
+      setLoadind(false)
+      setError(error.message)
+    }
+  }
   return (
     <div className="flex items-center justify-center min-h-[90vh]">
       <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="username"
@@ -14,10 +49,12 @@ export default function SignUp() {
               Username
             </label>
             <input
+              required
               type="text"
               id="username"
               name="username"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -27,11 +64,12 @@ export default function SignUp() {
             >
               Email
             </label>
-            <input
+            <input required
               type="email"
               id="email"
               name="email"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-6">
@@ -41,19 +79,20 @@ export default function SignUp() {
             >
               Password
             </label>
-            <input
+            <input required
               type="password"
               id="password"
               name="password"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
           <div className="flex items-center justify-center">
-            <button
+            <button disabled={loadind}
               type="submit"
               className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Sign Up
+              {loadind? 'loading...': 'SIGN UP'}
             </button>
           </div>
         </form>
@@ -63,6 +102,7 @@ export default function SignUp() {
             Sign In
           </a>
         </p>
+        {error && <p className="text-red-600 mt-5"> {'Username or email already taken.'} </p>}
       </div>
     </div>
   );
