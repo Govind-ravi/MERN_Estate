d9ddf14ffe7bf5e0b16f0113f2675e43f78c2285
Govind-ravi <govindnr20122001@gmail.com>
2024-07-06 11:06:39 +0530

    Complete signIn page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,45 +1,93 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState();
+  const [loadind, setLoadind] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  }
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try {
+      
+      setLoadind(true);
+      const res = await fetch('api/auth/signin', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if(data.success === false){
+        setError(data.message);
+        setLoadind(false);
+        return;
+      }
+      setLoadind(false);
+      setError(null);
+      navigate('/')
+    } catch (error) {
+      setLoadind(false)
+      setError(error.message)
+    }
+  }
   return (
-    <div className="flex items-center justify-center h-[90vh]">
+    <div className="flex items-center justify-center min-h-[90vh]">
       <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-            <input 
-              type="email" 
-              id="email" 
-              name="email" 
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input required
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-            <input 
-              type="password" 
-              id="password" 
-              name="password" 
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input required
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              onChange={handleChange}
             />
           </div>
           <div className="flex items-center justify-center">
-            <button 
-              type="submit" 
+            <button disabled={loadind}
+              type="submit"
               className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Sign In
+              {loadind? 'loading...': 'SIGN IN'}
             </button>
           </div>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
-          <a href="/signup" className="text-indigo-600 hover:text-indigo-500">Sign Up</a>
+          Don't have an account?{" "}
+          <a href="/signup" className="text-indigo-600 hover:text-indigo-500">
+            Sign Up
+          </a>
         </p>
+        {error && <p className="text-red-600 mt-5"> {'Wrong Credentials!'} </p>}
       </div>
     </div>
-  )
+  );
 }
