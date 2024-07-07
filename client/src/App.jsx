5e20c644164fa58c830e8a49569c2a4a56248150
Govind-ravi <govindnr20122001@gmail.com>
2024-07-07 22:14:37 +0530

    Profile UI complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return <BrowserRouter>
@@ -12,9 +13,11 @@ export default function App() {
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/about' element={<About/>} />
-    <Route path='/profile' element={<Profile/>} />
-    <Route path='/sign-in' element={<SignIn/>} />
     <Route path='/signup' element={<SignUp/>} />
+    <Route path='/sign-in' element={<SignIn/>} />
+    <Route element={<PrivateRoute/>} >
+     <Route path='/profile' element={<Profile/>} />
+    </Route>
   </Routes>
   </BrowserRouter>
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import {useSelector} from 'react-redux'
 
 export default function Profile() {
+  const {currentUser} =useSelector((state)=> state.user)
   return (
-    <div>
-      Profile
+    <div className="flex items-center justify-center min-h-[90vh]">
+      <div className="w-full max-w-md bg-white p-8 rounded shadow-md flex flex-col">
+        <h2 className="text-2xl font-bold mb-6 text-center">Profile</h2>
+        <img src={currentUser.avatar} alt="ProfilePic" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center' />
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              defaultValue={currentUser.username}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              defaultValue={currentUser.email}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              defaultValue="CURRENT_USER_PASSWORD"
+            />
+          </div>
+          <div className="flex justify-center mb-6">
+            <button
+              type="submit"
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-80"
+            >
+              Update
+            </button>
+          </div>
+          </form>
+          <div className="flex justify-between mt-4">
+            <button
+              type="button"
+              className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Delete Account
+            </button>
+            <button
+              type="button"
+              className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
+              Sign Out
+            </button>
+          </div>
+      </div>
     </div>
-  )
-}
+  );
+  
+  }
