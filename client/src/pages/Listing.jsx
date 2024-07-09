22b944a449b300ab6ed3de2b92cd0e8638e32ae3
Govind-ravi <govindnr20122001@gmail.com>
2024-07-09 06:14:18 +0530

    Some Changes

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -11,15 +11,17 @@ export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [showImagesModal, setShowImagesModal] = useState(false);
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    autoplay: false,
+    arrows: true,
+    adaptiveHeight: true,
   };
 
   useEffect(() => {
@@ -46,39 +48,51 @@ export default function Listing() {
 
   const images = listing.imageURLs; // Use the images from the fetched listing
 
+  const openImagesModal = () => {
+    setShowImagesModal(true);
+  };
+
+  const closeImagesModal = () => {
+    setShowImagesModal(false);
+  };
+
   return (
-    <div>
+    <div className="relative">
       {/* Image Slider */}
       <div className="w-full h-[50vh]">
         <Slider {...settings}>
           {images.map((image, index) => (
             <div key={index}>
-              <img src={image} alt={`Slide ${index}`} className="w-full h-[50vh] object-cover" />
+              <img
+                src={image}
+                alt={`Slide ${index}`}
+                className="w-full h-[50vh] object-cover"
+              />
             </div>
           ))}
         </Slider>
       </div>
 
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-4xl mx-auto relative">
         {/* Title and Price */}
         <div className="flex justify-between items-center mt-4">
-          <h1 className="text-3xl font-bold">
-            {listing.name}
-          </h1>
+          <h1 className="text-3xl font-bold">{listing.name}</h1>
           <span className="text-3xl font-bold">
-            ${listing.regularPrice}{listing.type === "rent" && "/month"}
+            ${listing.regularPrice}
+            {listing.type === "rent" && "/month"}
           </span>
         </div>
         {/* Discount Price */}
         {listing.offer && (
           <div className="text-xl text-red-500 mt-2">
-            Offer Price: ${listing.discountPrice}{listing.type === "rent" && "/month"}
+            Offer Price: ${listing.discountPrice}
+            {listing.type === "rent" && "/month"}
           </div>
         )}
 
         {/* Rent Box */}
-        <div className="bg-green-500 text-white px-4 py-2 mt-2 inline-block rounded">
-          {listing.type === "rent" ? "Rent" : "Sale"}
+        <div className="bg-green-700 text-white px-4 py-2 mt-2 inline-block rounded font-semibold">
+          {listing.type === "rent" ? "For Rent" : "For Sale"}
         </div>
 
         {/* Address */}
@@ -94,11 +108,17 @@ export default function Listing() {
         <div className="mt-4 grid grid-cols-2 gap-4">
           <div className="flex items-center text-gray-700">
             <FaBed className="mr-2" />
-            <span>{listing.bedrooms} Beds</span>
+            <span>
+              {listing.bedrooms}
+              {listing.bedrooms > 1 ? " Beds" : " Bed"}
+            </span>
           </div>
           <div className="flex items-center text-gray-700">
             <FaBath className="mr-2" />
-            <span>{listing.bathrooms} Baths</span>
+            <span>
+              {listing.bathrooms}
+              {listing.bathrooms > 1 ? " Baths" : " Bath"}
+            </span>
           </div>
           <div className="flex items-center text-gray-700">
             <FaParking className="mr-2" />
@@ -109,6 +129,41 @@ export default function Listing() {
             <span>{listing.furnished ? "Furnished" : "Not Furnished"}</span>
           </div>
         </div>
+
+        {/* View all images button */}
+        <div className="fixed bottom-4 right-4">
+          <button
+            className="flex items-center bg-indigo-900 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none"
+            onClick={openImagesModal}
+          >
+            View All Images
+          </button>
+        </div>
+
+        {/* Images Modal */}
+        {showImagesModal && (
+          <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center">
+            <div className="relative w-[60vw] h-[80vh]">
+              <Slider {...settings}>
+                {images.map((image, index) => (
+                  <div key={index}>
+                    <img
+                      src={image}
+                      alt={`Slide ${index}`}
+                      className="w-full h-[80vh] object-contain"
+                    />
+                  </div>
+                ))}
+              </Slider>
+              <button
+                className="absolute top-2 right-2 text-white text-xl focus:outline-none"
+                onClick={closeImagesModal}
+              >
+                &times;
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
